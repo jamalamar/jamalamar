ef0dbf44a4f9236d4b9b988b1d084bb442da4b50
jamalamar <jamal_ab22@hotmail.com>
2019-03-24 16:02:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+	// router.get('/about',function(req,res){
+	//   res.sendFile(path.join(__dirname+'/about.html'));
+	// });
+
+
+	// router.get('/sitemap',function(req,res){
+	//   res.sendFile(path.join(__dirname+'/sitemap.html'));
+	// });
+
+
+//add the router
+app.use(express.static(__dirname + '/View'));
+//Store all HTML files in view folder.
+
+app.use(express.static(__dirname + '/Scripts'));
+//Store all JS and CSS in Scripts folder.
+
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
